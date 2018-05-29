78fa29cb8f7bf5392f95b95244dbf8b86a6a1219
fabian <fabian.zamudio.89@gmail.com>
2018-05-28 21:56:19 -0300

    Empty application MVC .Net Core 2.0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvc_application_net_core_2._0_final_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
